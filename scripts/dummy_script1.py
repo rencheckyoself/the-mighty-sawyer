804e44289de9cf013d246033fd9c6ac0992099de
lee-jm <jmlee@u.northwestern.edu>
2019-11-21 22:03:18 -0600

    added some dummy files to layout package structure: part2

---
+++ scripts/dummy_script1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""
+Scripts in this folder act mainly as a lightweight script that imports stuff from the subpackage (eg. the-mighty-sawyer/src/the_mighty_sawyer) -- where all the cool stuff is really happening.
+"""
+from __future__ import print_function
+from the_mighty_sawyer import function_x_from_dummy_module1, Class_Y_from_dummy_module1
+
+def lightweight():
+	pass
+
+if __name__ == "__main__":
+    
---
+++ src/the_mighty_sawyer/dummy_module1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+def Class_Y_from_dummy_module1(object):
+	def d(self):
+		""" Dummy class """
+		return "This is a dummy class. This is where all the cool stuff is really happening that can be imported in /nodes."
+
+def function_x_from_dummy_module1():
+	pass
